5f11ba92f904479e484d899307c00c9e4d32e4a3
Olexander Hofman <oh@mamoru.network>
2023-04-10 15:58:01 +0200

    MAMORU-327: Create Solidity Registry Smart contract 0.1 Add test for EthMamoruRelay contract

--- eth_mamoru_relay/contracts/EthMamoruRelay.sol
+++ eth_mamoru_relay/contracts/EthMamoruRelay.sol
@@ -4,15 +4,6 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
-//todo
-//interface MamoruRelayInterface {
-//    function addValidator(address _validator) public;
-//    function removeValidator(address _validator) public;
-//    function addIncident(uint256 _daemonId, uint256 _incidentId, Incident memory _incident) public;
-//    function getIncidentsSinceByDaemon(uint256 _daemonId, uint256 _sinceTimestamp) public view returns (Incident[] memory);
-//    function hasIncidents(uint256 _daemonId, uint256 _sinceTimestamp) public view returns (bool);
-//}
-
 contract EthMamoruRelay is Ownable {
 
     uint public incidentCount = 0;
@@ -51,26 +42,17 @@ contract EthMamoruRelay is Ownable {
     }
 
     function addIncident(string memory daemonId, Incident memory _incident) public {
+        console.log("Check validator address: %s", msg.sender);
         require(validators[msg.sender], "Only validators can report incidents.");
         // add the incident to the registry
         daemonIncidents[daemonId][incidentCount] = _incident;
 
         emit IncidentReported(daemonId, _incident.Id);
-        console.log("Incident reported: daemonId=%s, incidentId=%d count=%d", daemonId, _incident.Id, incidentCount);
-
         incidentCount++;
+        console.log("Incident reported: daemonId=%s, incidentId=%d count=%d", daemonId, _incident.Id, incidentCount);
     }
 
-//    function getIncidentsByDaemon(uint256 daemonId) public view returns (Incident[] memory) {
-//        // return a list of incidents for the given daemon
-//        Incident[] memory incidents = new Incident[](incidentCount);
-//        for (uint256 i = 0; i < incidentCount; i++) {
-//            incidents[i] = daemonIncidents[daemonId][i];
-//        }
-//        return incidents;
-//    }
-
-    function getIncidentsSinceByDaemon(string memory daemonId, uint64 sinceTimestamp) public view returns (Incident[] memory) {
+    function getIncidentsSinceByDaemon(string memory daemonId, uint64 sinceTimestamp) public view returns (Incident[] memory, uint256) {
         // return a list of incidents for the given daemon since the given timestamp
         uint256 count = 0;
         for (uint256 i = 0; i < incidentCount; i++) {
@@ -78,7 +60,6 @@ contract EthMamoruRelay is Ownable {
                 count++;
             }
         }
-        console.log("Incidents since %s: %d", sinceTimestamp, count);
         Incident[] memory incidents = new Incident[](count);
         uint256 index = 0;
         for (uint256 i = 0; i < incidentCount; i++) {
@@ -88,7 +69,7 @@ contract EthMamoruRelay is Ownable {
             }
         }
         console.log("Incidents since %s: %d", sinceTimestamp, count);
-        return incidents;
+        return (incidents, count);
     }
 
     function hasIncidents(string memory daemonId, uint256 sinceTimestamp) public view returns (bool) {
--- eth_mamoru_relay/test/EthMamoruRelay.ts
+++ eth_mamoru_relay/test/EthMamoruRelay.ts
@@ -1,9 +1,10 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
+
+describe("EthMamoruRelay", function () {
+
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
@@ -19,6 +20,8 @@ describe("Lock", function () {
   }
 
   describe("Deployment", function () {
+    const timestamp = Math.floor(Date.now() / 1000);
+
     it("Should set the right incidentCount", async function () {
       const { relay } = await loadFixture(deployOneYearLockFixture);
 
@@ -31,90 +34,170 @@ describe("Lock", function () {
       expect(await relay.owner()).to.equal(owner.address);
     });
 
-  //   it("Should receive and store the funds to lock", async function () {
-  //     const { lock, lockedAmount } = await loadFixture(
-  //       deployOneYearLockFixture
-  //     );
-  //
-  //     expect(await ethers.provider.getBalance(lock.address)).to.equal(
-  //       lockedAmount
-  //     );
-  //   });
-  //
-  //   it("Should fail if the unlockTime is not in the future", async function () {
-  //     // We don't use the fixture here because we want a different deployment
-  //     const latestTime = await time.latest();
-  //     const Lock = await ethers.getContractFactory("Lock");
-  //     await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-  //       "Unlock time should be in the future"
-  //     );
-  //   });
-  // });
-  //
-  // describe("Withdrawals", function () {
-  //   describe("Validations", function () {
-  //     it("Should revert with the right error if called too soon", async function () {
-  //       const { lock } = await loadFixture(deployOneYearLockFixture);
-  //
-  //       await expect(lock.withdraw()).to.be.revertedWith(
-  //         "You can't withdraw yet"
-  //       );
-  //     });
-  //
-  //     it("Should revert with the right error if called from another account", async function () {
-  //       const { lock, unlockTime, otherAccount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-  //
-  //       // We can increase the time in Hardhat Network
-  //       await time.increaseTo(unlockTime);
-  //
-  //       // We use lock.connect() to send a transaction from another account
-  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-  //         "You aren't the owner"
-  //       );
-  //     });
-  //
-  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-  //       const { lock, unlockTime } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-  //
-  //       // Transactions are sent using the first signer by default
-  //       await time.increaseTo(unlockTime);
-  //
-  //       await expect(lock.withdraw()).not.to.be.reverted;
-  //     });
-  //   });
-  //
-  //   describe("Events", function () {
-  //     it("Should emit an event on withdrawals", async function () {
-  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-  //
-  //       await time.increaseTo(unlockTime);
-  //
-  //       await expect(lock.withdraw())
-  //         .to.emit(lock, "Withdrawal")
-  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-  //     });
-  //   });
-  //
-  //   describe("Transfers", function () {
-  //     it("Should transfer the funds to the owner", async function () {
-  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-  //
-  //       await time.increaseTo(unlockTime);
-  //
-  //       await expect(lock.withdraw()).to.changeEtherBalances(
-  //         [owner, lock],
-  //         [lockedAmount, -lockedAmount]
-  //       );
-  //     });
-  //   });
+    it("Should add the validator and emit an event", async function () {
+      const { relay, otherAccount } = await loadFixture(
+          deployOneYearLockFixture
+      );
+
+      await expect(relay.addValidator(otherAccount.address)).to
+          .emit(relay, "ValidatorAdded").withArgs(otherAccount.address);
+    });
+
+    it("Should remove the validator and emit an event", async function () {
+      const { relay, otherAccount } = await loadFixture(
+          deployOneYearLockFixture
+      );
+      await relay.addValidator(otherAccount.address);
+      await expect(relay.removeValidator(otherAccount.address)).to
+          .emit(relay, "ValidatorRemoved").withArgs(otherAccount.address);
+    });
+
+    it("should allow validators to report incidents", async function () {
+      // Deploy a mock validator
+      const { relay, otherAccount} = await loadFixture(
+          deployOneYearLockFixture
+      );
+       const [not_validator] = await ethers.getSigners();
+
+        // Add the validator
+      await expect(relay.addValidator(otherAccount.address)).to
+          .emit(relay, "ValidatorAdded").withArgs(otherAccount.address);
+
+      // Define a test incident
+      const incident = {
+        Id: "1",
+        Address: otherAccount.address,
+        Data: "0x0000000000000000000000000000000000000000",
+        CreatedAt: timestamp,
+      };
+
+      // Call the addIncident function
+      const daemonId = "test-daemon";
+
+      await expect(relay.connect(not_validator).addIncident(daemonId, incident)).to.be
+          .revertedWith("Only validators can report incidents.");
+
+      await expect(await relay.connect(otherAccount).addIncident(daemonId, incident)).to
+          .emit(relay, "IncidentReported").withArgs(daemonId, incident.Id);
+
+      // Check that the incident count was incremented
+      const incidentCount = await relay.incidentCount();
+      expect(incidentCount.toNumber()).to.equal(1);
+
+      // Check that the incident was added to the registry
+      const [storedIncidents, count] = await relay.getIncidentsSinceByDaemon(daemonId, incident.CreatedAt);
+
+      expect(count.toNumber()).to.equal(1);
+      expect(storedIncidents).to.have.lengthOf(1);
+
+      expect(storedIncidents[0].Id).to.equal(incident.Id);
+      expect(storedIncidents[0].Address).to.equal(incident.Address);
+      expect(storedIncidents[0].Data).to.equal(incident.Data);
+      expect(storedIncidents[0].CreatedAt).to.equal(incident.CreatedAt);
+    });
+
+    it("should not allow non-validators to report incidents", async function () {
+      // Deploy a mock validator
+      const { relay} = await loadFixture(
+          deployOneYearLockFixture
+      );
+      const [nonValidator] = await ethers.getSigners();
+
+      // Define a test incident
+      const incident = {
+        Id: "1",
+        Address: nonValidator.address,
+        Data: "0x0000000000000000000000000000000000000000",
+        CreatedAt: 1623469105
+      };
+
+      // Call the addIncident function as a non-validator
+      const daemonId = "test-daemon";
+      await expect(relay.connect(nonValidator).addIncident(daemonId, incident)).to.be
+          .revertedWith("Only validators can report incidents.");
+
+      // Check that the incident was not added to the registry
+      const incidentCount = await relay.incidentCount();
+      expect(incidentCount.toNumber()).to.equal(0);
+    });
 
   });
+
+  describe("getIncidentsSinceByDaemon", () => {
+    const daemonId = "testDaemon";
+    const timestamp = Math.floor(Date.now() / 1000);
+
+    it("should return empty array when no incidents reported", async () => {
+      // Deploy a mock validator
+      const { relay} = await loadFixture(
+          deployOneYearLockFixture
+      );
+      const [incidents, count] = await relay.getIncidentsSinceByDaemon(daemonId, timestamp);
+      expect(count).to.equal(0);
+      expect(incidents).to.have.lengthOf(0);
+    });
+
+    it("should return array of incidents reported by a daemon since the timestamp", async () => {
+      // Deploy a mock validator
+      const { relay, owner} = await loadFixture(
+          deployOneYearLockFixture
+      );
+      await relay.addValidator(owner.address)
+
+      const incident1 = {Id: "incident1", Address: owner.address, Data: "0x0000000000000000000000000000000000000001", CreatedAt: timestamp - 100};
+      const incident2 = {Id: "incident2", Address: owner.address, Data: "0x0000000000000000000000000000000000000002", CreatedAt: timestamp + 50};
+      const incident3 = {Id: "incident3", Address: owner.address, Data: "0x0000000000000000000000000000000000000003", CreatedAt: timestamp + 10};
+
+      await relay.addIncident(daemonId, incident1);
+      await relay.addIncident(daemonId, incident2);
+      await relay.addIncident(daemonId, incident3);
+
+      const [incidents, count] = await relay.getIncidentsSinceByDaemon(daemonId, timestamp);
+
+      expect(count.toNumber()).to.equal(2);
+      expect(incidents).to.have.lengthOf(2);
+
+      expect(incidents[0].Id).to.deep.equal(incident2.Id);
+      expect(incidents[0].Address).to.deep.equal(incident2.Address);
+      expect(incidents[0].Data).to.deep.equal(incident2.Data);
+      expect(incidents[0].CreatedAt.toNumber()).to.deep.equal(incident2.CreatedAt);
+
+      expect(incidents[1].Id).to.deep.equal(incident3.Id);
+      expect(incidents[1].Address).to.deep.equal(incident3.Address);
+      expect(incidents[1].Data).to.deep.equal(incident3.Data);
+      expect(incidents[1].CreatedAt.toNumber()).to.deep.equal(incident3.CreatedAt);
+    });
+
+    it("should not include incidents reported by other daemons", async () => {
+      // Deploy a mock validator
+      const { relay, owner} = await loadFixture(
+          deployOneYearLockFixture
+      );
+      await relay.addValidator(owner.address)
+
+      const incident1 = {Id: "incident1", Address: owner.address, Data: "0x0000000000000000000000000000000000000001", CreatedAt: timestamp + 100};
+      const incident2 = {Id: "incident2", Address: owner.address, Data: "0x0000000000000000000000000000000000000002", CreatedAt: timestamp + 50};
+      const incident3 = {Id: "incident3", Address: owner.address, Data: "0x0000000000000000000000000000000000000003", CreatedAt: timestamp + 10};
+
+      await relay.addIncident(daemonId, incident1);
+      await relay.addIncident("otherDaemon", incident2);
+      await relay.addIncident(daemonId, incident3);
+      //
+      const [incidents, count] = await relay.getIncidentsSinceByDaemon(daemonId, timestamp);
+      expect(count.toNumber()).to.equal(2);
+      expect(incidents).to.have.lengthOf(2);
+
+      expect(incidents[0].Id).to.equal(incident1.Id);
+      expect(incidents[0].Address).to.equal(incident1.Address);
+      expect(incidents[0].Data).to.equal(incident1.Data);
+      expect(incidents[0].CreatedAt.toNumber()).to.equal(incident1.CreatedAt);
+
+      expect(incidents[1].Id).to.equal(incident3.Id);
+      expect(incidents[1].Address).to.equal(incident3.Address);
+      expect(incidents[1].Data).to.equal(incident3.Data);
+      expect(incidents[1].CreatedAt.toNumber()).to.equal(incident3.CreatedAt);
+    });
+
+  });
+
 });

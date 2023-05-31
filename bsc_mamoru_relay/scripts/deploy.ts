37d8f87fdcd5f04aa732de77c5ac5e3e4d7ada86
Olexander Hofman <oh@mamoru.network>
2023-05-31 16:21:54 +0200

    -added bsc smart contract deploy

---
+++ bsc_mamoru_relay/scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const RelayContract = await ethers.getContractFactory("BscMamoruRelay");
+  const relay = await RelayContract.deploy();
+  await relay.deployed();
+
+  console.log(
+    `${relay.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
